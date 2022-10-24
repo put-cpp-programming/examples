62f727f084b9ec9f87c8cc8a8ae9f33340beff8b
Piotr Kontowicz <piotr.kontowicz@put.poznan.pl>
2022-10-24 14:30:43 +0200

    Update

---
+++ Lab04/01/main.h
@@ -0,0 +1,8 @@
+﻿// main.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream> // Header iostream is included in this file, and this file is included in main.cpp. So iostream library is available in main.cpp file.
+
+// TODO: Reference additional headers your program requires here.
